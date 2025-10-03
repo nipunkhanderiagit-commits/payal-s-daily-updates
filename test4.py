d641b8714dec68e3680bae626b3d8e516678422e
nipunkhanderia <nipunkhanderia@gmail.com>
2025-10-03 21:45:56 +0100

    2nd

--- test4.py
+++ test4.py
@@ -2,6 +2,8 @@ from ollama import Client
 
 client = Client(host  = "http://localhost:11434")
 
+
+
 response = client.chat(model = "llama3.2", messages = [{"role":"user", "content":"Tell me about jainism"}])
 
 print(response)
